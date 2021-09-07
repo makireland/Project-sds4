d3d513b64e73f0beae78fed085ba9726746393ae
makireland <makireland@gmail.com>
2021-09-07 03:30:13 +0100

    Created components Navbar and Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
-  return (
-    <div>
-      <h1 className="text-primary">Ola Mundo!</h1>
-    </div>
-  );
+    return (
+        <>
+            <NavBar />
+            <div className = "container">
+                <h1 className="text-primary">Ola Mundo!</h1>
+            </div>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
